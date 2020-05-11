06161f91a8a9cc5883c610e8545355d826291d18
Lauren Mengert <laurenmengert@Laurens-MacBook-Pro.local>
2020-05-11 09:08:51 -0600

    Shuffle Function Complete

---
+++ js/deck.js
@@ -0,0 +1,211 @@
+const deck = [
+    {
+        name: "card dA",
+        value: 14
+    },
+    {
+        name: "card dK",
+        value: 13
+    },
+    {
+        name: "card dQ",
+        value: 12
+    },
+    {
+        name: "card dJ",
+        value: 11
+    },
+    {
+        name: "card d10",
+        value: 10
+    },
+    {
+        name: "card d09",
+        value: 9
+    },
+    {
+        name: "card d08",
+        value: 8
+    },
+    {
+        name: "card d07",
+        value: 7
+    },
+    {
+        name: "card d06",
+        value: 6
+    },
+    {
+        name: "card d05",
+        value: 5
+    },
+    {
+        name: "card d04",
+        value: 4
+    },
+    {
+        name: "card d03",
+        value: 3
+    },
+    {
+        name: "card d02",
+        value: 2
+    },
+    {
+        name: "card dA",
+        value: 14
+    },
+    {
+        name: "card dK",
+        value: 13
+    },
+    {
+        name: "card dQ",
+        value: 12
+    },
+    {
+        name: "card dJ",
+        value: 11
+    },
+    {
+        name: "card d10",
+        value: 10
+    },
+    {
+        name: "card d09",
+        value: 9
+    },
+    {
+        name: "card d08",
+        value: 8
+    },
+    {
+        name: "card d07",
+        value: 7
+    },
+    {
+        name: "card d06",
+        value: 6
+    },
+    {
+        name: "card d05",
+        value: 5
+    },
+    {
+        name: "card d04",
+        value: 4
+    },
+    {
+        name: "card d03",
+        value: 3
+    },
+    {
+        name: "card d02",
+        value: 2
+    },
+    {
+        name: "card dA",
+        value: 14
+    },
+    {
+        name: "card dK",
+        value: 13
+    },
+    {
+        name: "card dQ",
+        value: 12
+    },
+    {
+        name: "card dJ",
+        value: 11
+    },
+    {
+        name: "card d10",
+        value: 10
+    },
+    {
+        name: "card d09",
+        value: 9
+    },
+    {
+        name: "card d08",
+        value: 8
+    },
+    {
+        name: "card d07",
+        value: 7
+    },
+    {
+        name: "card d06",
+        value: 6
+    },
+    {
+        name: "card d05",
+        value: 5
+    },
+    {
+        name: "card d04",
+        value: 4
+    },
+    {
+        name: "card d03",
+        value: 3
+    },
+    {
+        name: "card d02",
+        value: 2
+    },
+    {
+        name: "card dA",
+        value: 14
+    },
+    {
+        name: "card dK",
+        value: 13
+    },
+    {
+        name: "card dQ",
+        value: 12
+    },
+    {
+        name: "card dJ",
+        value: 11
+    },
+    {
+        name: "card d10",
+        value: 10
+    },
+    {
+        name: "card d09",
+        value: 9
+    },
+    {
+        name: "card d08",
+        value: 8
+    },
+    {
+        name: "card d07",
+        value: 7
+    },
+    {
+        name: "card d06",
+        value: 6
+    },
+    {
+        name: "card d05",
+        value: 5
+    },
+    {
+        name: "card d04",
+        value: 4
+    },
+    {
+        name: "card d03",
+        value: 3
+    },
+    {
+        name: "card d02",
+        value: 2
+    },
+]
+
--- js/main.js
+++ js/main.js
@@ -1,17 +1,43 @@
 /*----- constants -----*/
-
+const suits = ['spades', 'hearts', 'clubs', 'diamonds'];
+const cardFace = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
 
 /*----- app's state (variables) -----*/
 
-
+let shuffledDeck;
+let players = [[],[]];
+let gameOver = false
 
 /*----- cached element references -----*/
 
+//const scoreEls = ;
+//const resultEls = ;
 
 
 /*----- event listeners -----*/
 
-
+document.querySelector("#war").addEventListener("click", playRound);
+document.querySelector("#reset").addEventListener("click", init);
 
 /*----- functions -----*/
+
+function init() {
+    
+};
+
+//pass "deck" through when you want to call this function
+function shuffleCards(arr) { 
+    for(let i = arr.length -1; i > 0; i--) {
+        let x = Math.floor(Math.random() * (i + 1));
+        let temp = arr[i];
+        arr[i] = arr[x];
+        arr[x] = temp;
+        console.log(temp);
+    }
+    return arr;
+}
+
+function playRound() {
+    
+};
